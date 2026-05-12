7b294762c1545c2e8518019ba71ccddc22975349
Priyaharini12 <priyaharini1206@gmail.com>
2026-05-12 20:01:02 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Priyaharini Palanisamy
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -10,18 +10,33 @@ module tt_um_example (
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    output wire [7:0] uio_oe,   // IOs: Enable path
+    input  wire       ena,      // always 1 when powered
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n     // active low reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg [3:0] count;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    // Up Counter
+    always @(posedge clk or negedge rst_n)
+    begin
+        if (!rst_n)
+            count <= 4'b0000;
+        else
+            count <= count + 1'b1;
+    end
+
+    // Output connections
+    assign uo_out = {4'b0000, count};
+
+    // Unused IOs
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
+
+    // Prevent unused signal warnings
+    wire _unused = &{ena, ui_in, uio_in, 1'b0};
 
 endmodule
+
+`default_nettype wire
